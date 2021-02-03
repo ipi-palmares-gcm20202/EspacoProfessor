5700c109c2e2f94c36fcf44685e45081a7096c99
Paulo César Lins <61951700+paulocsarlins@users.noreply.github.com>
2021-02-03 06:29:38 +0300

    Atualiza cor da barra de navegação

--- espaco-professor/src/components/MenuSuperior.vue
+++ espaco-professor/src/components/MenuSuperior.vue
@@ -1,57 +1,59 @@
 <template>
-    <nav class="bar-nav">
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo">
-                <img class="img-logo" src="https://i.ibb.co/LQ5myXf/logo-espaco-professor.png" alt="">
-            </a>
-            <a href="#" data-activates="menu-mobile" class="button-collapse">
-                <i class="material-icons">menu</i>
-            </a>
-            <ul class="right hide-on-med-and-down">
-                <li><a v-on:click="TelaInicio">Início</a></li>
-                <li><a v-on:click="TelaLogin">Login</a></li>
-                <li><a v-on:click="TelaCadastro">Cadastrar</a></li>
-                <li><a href="#"></a></li>
-            </ul>
-        </div>
-    </nav>
+  <nav class="bar-nav">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">
+        <img
+          class="img-logo"
+          src="https://i.ibb.co/LQ5myXf/logo-espaco-professor.png"
+          alt=""
+        />
+      </a>
+      <a href="#" data-activates="menu-mobile" class="button-collapse">
+        <i class="material-icons">menu</i>
+      </a>
+      <ul class="right hide-on-med-and-down">
+        <li><a v-on:click="TelaInicio">Início</a></li>
+        <li><a v-on:click="TelaLogin">Login</a></li>
+        <li><a v-on:click="TelaCadastro">Cadastrar</a></li>
+        <li><a href="#"></a></li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-    name: 'menu-superior',
-    methods: {
-    TelaCadastro(){
-      this.$router.push({ name: 'Cadastro'})
+  name: "menu-superior",
+  methods: {
+    TelaCadastro() {
+      this.$router.push({ name: "Cadastro" });
     },
-    TelaLogin(){
-      this.$router.push({ name: 'Login'})
+    TelaLogin() {
+      this.$router.push({ name: "Login" });
     },
-    TelaInicio(){
-      this.$router.push({ name: 'Inicio'})
-    }
-
-    }
-}
+    TelaInicio() {
+      this.$router.push({ name: "Inicio" });
+    },
+  },
+};
 </script>
 
 <style scoped>
- .bar-nav{
-            background-color: white;
-        }
+.bar-nav {
+  background-color: #E6E6FA;
+}
 
-        .img-logo{
-            width: 150px;
-            height: 150px;
-            margin: -46px 0px 0px 20px;
-        }
+.img-logo {
+  width: 150px;
+  height: 150px;
+  margin: -46px 0px 0px 20px;
+}
 
-        .right {
-            margin-right: 50px;
-        }
+.right {
+  margin-right: 50px;
+}
 
-        ul li a{
-            color: black;
-        }
-     
+ul li a {
+  color: black;
+}
 </style>
